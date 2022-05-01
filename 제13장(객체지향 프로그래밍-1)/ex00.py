657cbac7b32a276ff1060b5ce05b25d1d99c5cbd
MabuhayGitHub <halflife@kakao.com>
2022-05-01 21:01:54 +0900

    2022.05.01.21.01

--- 제13장(객체지향 프로그래밍-1)/ex00.py
+++ 제13장(객체지향 프로그래밍-1)/ex00.py
@@ -2,6 +2,10 @@ class car:
     color = ""
     speed = 0
 
+    def __init__(self):
+        self.color = "WHITE"
+        self.speed = 0
+
     def upSpeed(self, value):
         self.speed += value
     
@@ -20,6 +24,8 @@ myCar3 = car()
 myCar3.color = "YELLOW"
 myCar3.speed = 0
 
+myCar4 = car()
+
 myCar1.upSpeed(30)
 print("자동차1의 색상은 %s이며, 현재 속도는 시속 %dkm입니다" % (myCar1.color, myCar1.speed))
 
@@ -28,3 +34,10 @@ print("자동차2의 색상은 %s이며, 현재 속도는 시속 %dkm입니다" 
 
 myCar1.upSpeed(0)
 print("자동차3의 색상은 %s이며, 현재 속도는 시속 %dkm입니다" % (myCar3.color, myCar3.speed))
+
+print("자동차4의 색상은 %s이며, 현재 속도는 시속 %dkm입니다" % (myCar4.color, myCar4.speed))
+
+# 자동차1의 색상은 RED이며, 현재 속도는 시속 30km입니다
+# 자동차2의 색상은 BLUE이며, 현재 속도는 시속 60km입니다 
+# 자동차3의 색상은 YELLOW이며, 현재 속도는 시속 0km입니다
+# 자동차4의 색상은 WHITE이며, 현재 속도는 시속 0km입니다
---
+++ 제13장(객체지향 프로그래밍-1)/person.py
@@ -0,0 +1,10 @@
+class Person:
+
+    name = ""
+    age = 0
+    height = 0
+    weight = 0
+    address = ""
+
+    def __init__(self):
+        self.name = "홍길동"
